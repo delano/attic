ec3fe27ecd629a32a8af026a217e68eb8da6fdf8
delano <delano@solutious.com>
2009-11-29 19:12:14 -0500

    Fixed tryout

--- try/40_explicit_accessor_tryouts.rb
+++ try/40_explicit_accessor_tryouts.rb
@@ -10,14 +10,14 @@ tryouts "Explicit accessors" do
   
   drill "can set value", 100 do
     a = Worker.new
-    a.attic_variable_set :size, 100
-    a.attic_variable_get :size
+    a.attic_variable_set :space, 100
+    a.attic_variable_get :space
   end
   
   drill "doesn't create accessor methods", false do
     a = Worker.new
-    a.attic_variable_set :size, 100
-    a.respond_to? :size
+    a.attic_variable_set :space, 100
+    a.respond_to? :space
   end
   
 end
