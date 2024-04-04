504c061d27f0a4b0756ee5fecb8af700a2131dac
delano <delano@cpan.org>
2024-04-04 13:13:31 -0700

    Fix tryouts for 1.0-RC1 naming

--- try/25_string_tryouts.rb
+++ try/25_string_tryouts.rb
@@ -4,25 +4,25 @@ require 'attic'
     String.extend Attic
     String.respond_to? :attic
 #=> true
-  
+
 ## save an instance variable the long way
 s = ""
-s.metametaclass.instance_variable_set '@mattress', 'S&F'
-s.metametaclass.instance_variable_get '@mattress'
+s.attic.instance_variable_set '@mattress', 'S&F'
+s.attic.instance_variable_get '@mattress'
 #=> 'S&F'
 
 ## can create attributes
 String.attic :goodies
 #=> [:goodies]
-  
+
 ## save an instance variable the short way
 s = ""
 s.goodies = :california_king
 p s.instance_variables
-p s.attic_vars
+p s.attic_variables
 s.goodies
 #=> :california_king
-  
+
 ## String instances don't cross streams
 String.extend Attic
 String.attic :name
